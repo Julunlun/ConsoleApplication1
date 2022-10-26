63810788130612c870f41b5ad8d468660840dd4c
10141 <10141@CLOWN>
2022-10-26 14:33:15 +0800

    添加项目文件。

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,21 @@
+﻿#include<stdio.h>
+#include<Windows.h>
+#include<iostream>
+using namespace std;
+int main() {
+	float x, y, a;
+	for (y = 1.5; y > -1.5; y -= 0.1)
+	{
+		for (x = -1.5; x < 1.5; x += 0.05)
+		{
+			a = x * x + y * y - 1;
+			putchar(a * a * a - x * x * y * y * y <= 0.0 ? 'y' : '  ');
+		}
+		system("color 0c");
+		putchar('\n');
+	}
+
+	cout << "输出完成" << '\n';
+	cin >> x;
+	return 0;
+}
